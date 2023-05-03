08f71959dfc6b61e25cac1bde2f359fd28fa9479
Dmitrii Kniazev <dmitriikniazev@gmail.com>
2023-05-03 18:40:59 +0400

    Add project files.

---
+++ MP3 Spectrograph Generator/Program.cs
@@ -0,0 +1,24 @@
+﻿using NAudio.Wave;
+
+namespace MP3_Spectrograph_Generator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the path to the MP3 file: ");
+            string filePath = Console.ReadLine();
+        }
+
+        static int GetSampleRate (string filePath)
+        {
+            
+            int sampleRate = 0;
+            Mp3Frame mp3FileFrame = null;
+
+            return sampleRate;
+        }
+    }
+
+
+}
